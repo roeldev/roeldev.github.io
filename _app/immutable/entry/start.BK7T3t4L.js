5a07a3e3a2338de914e455e6da84f42db1a62836
roeldev <roeldev@users.noreply.github.com>
2025-11-17 15:05:26 +0100

    new site live

---
+++ _app/immutable/entry/start.BK7T3t4L.js
@@ -0,0 +1,1 @@
+import{l as o,a as r}from"../chunks/ak9lD8eQ.js";export{o as load_css,r as start};
---
+++ _app/immutable/nodes/6.B8Mfv7Hu.js
@@ -0,0 +1,22 @@
+import{f as a,a as n}from"../chunks/DO6EhnEx.js";import"../chunks/DPRkqhqy.js";import{C as r}from"../chunks/CbcILUiY.js";var i=a(`<div class="w-8/10 mx-auto content"><h1>New website live</h1> <time datetime="2025-11-17">17/11/2025</time> <p>Welcome to my new website! It truly has been a long time coming. After a short downtime of almost 6 years, it's
+			time for a comeback.</p> <h2 id="introduction">Introduction</h2> <p>Let me briefly introduce myself. Hi, I'm Roel, a software and DevOps engineer from the Netherlands. Over the last
+			20 years I have built up experience in various programming languages and environments. Currently, my main focus is
+			on developing cloud-native/distributed applications using Go and container technologies such as Docker and
+			Kubernetes. I'm a big fan of the <a href="https://12factor.net/" target="_blank" rel="nofollow noopener">12factor
+				app</a> and use DevOps/GitOps principles wherever they are a good fit.</p> <p>I'm also a creator/maintainer of several opensource <a href="https://github.com/roeldev" target="_blank" rel="nofollow noopener">projects</a> and <a href="https://github.com/go-pogo" target="_blank" rel="nofollow noopener">packages</a>, and I love to tinker with
+			all kinds of tech-related stuff. Of course, as a tech enthusiast, I also have an overpowered homelab and a
+			decent amount of smart home tech installed in my house. The homelab runs Kubernetes based on Talos Linux and was
+			featured on <a href="https://www.siderolabs.com/blog/talos-home-lab-clusters/" target="_blank" rel="nofollow noopener">Sidero Labs' blog</a> (scroll to the "Supermicro + Proxmox" heading). This is actually
+			an older setup which has changed quite a bit; more on that later...</p> <h2 id="a-long-time-ago">A long time ago...</h2> <p>According to the whois information, I registered the domain name roelschut.nl on February 4th of 2012. Looking at
+			the Wayback Machine, the first actual website appeared not until <a href="https://web.archive.org/web/20141012214452/http://roelschut.nl/" target="_blank" rel="nofollow noopener">October
+				2014</a>. Before that it would redirect any visitor to another website of mine, <a href="https://web.archive.org/web/20140109220523/http://roelschut-fotografie.nl/" target="_blank" rel="nofollow noopener">roelschut-fotografie.nl</a>.</p> <p>Several different iterations using different backends/frontends have been in development, but none of them ever
+			really went past the alpha phase. Somewhere in January of 2019 I decided to just pull the plug and redirect the
+			domain to my LinkedIn page. Frontend development was not something I enjoyed much anymore. The focus of my career
+			shifted from being a full-stack web developer to a backend developer to a software and DevOps engineer.</p> <h2 id="the-future">The future</h2> <p>This time I'll try to keep it relatively simple so I can focus on creating some interesting content. So (note to
+			self), I won't be using a (self-created) CMS or any other fancy backend solutions. It's just TailwindCSS for basic
+			styling together with Svelte, which I really like so far. Actually, as of writing, the website is statically
+			generated using SvelteKit and is hosted using GitHub Pages. A pretty simple solution until I find a good way to
+			safely expose my internal Kubernetes cluster to the big bad internet.</p> <p>Content I want to write about are some how-to/tutorial-based articles on a more regular basis, based on
+			techniques/tools I use myself. The first is almost finished and features setting up Traefik with a
+			custom root certificate on TrueNAS. Another one I'm working on will be about creating a Talos Kubernetes cluster
+			on bare-metal machines using Omni.</p> <p>I've got a gazillion other ideas, so if you're into this kind of stuff, make sure to keep an eye out :)</p></div>`);function d(e){r(e,{children:(t,s)=>{var o=i();n(t,o)},$$slots:{default:!0}})}export{d as component};
